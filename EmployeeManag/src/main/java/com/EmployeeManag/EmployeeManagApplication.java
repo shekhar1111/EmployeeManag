28822e0883447966c786b7edd2ba1eeb0d9141dc
shekhar <jundalesomshekhar@gmail.com>
2021-07-28 14:57:01 +0530

    initial commit

---
+++ EmployeeManag/src/main/java/com/EmployeeManag/EmployeeManagApplication.java
@@ -0,0 +1,13 @@
+package com.EmployeeManag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagApplication.class, args);
+	}
+
+}
